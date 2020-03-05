486cc994e0c0d45960dbe19fe76f0f65a8e4f928
Mohamed Abady <mohamed.abady995@gmail.com>
2020-03-05 22:38:02 +0200

    Initialize Tadbeer App

---
+++ android/app/src/main/java/com/tadbeerapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tadbeerapp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "TadbeerApp";
+  }
+}
