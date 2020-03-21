9e775e5f9bacc5c31aa2a7e9ffe77a6905940a04
Mohamed Abady <mohamed.abady995@gmail.com>
2020-03-21 23:35:20 +0200

    completed worker profile

--- SRC/Screens/WorkerProfile/WorkerProfile.js
+++ SRC/Screens/WorkerProfile/WorkerProfile.js
@@ -39,6 +39,33 @@ export default class WorkerProfile extends Component {
         },
     ]
 
+    packages = [
+        {
+            id: 0,
+            name: 'Traditional Package',
+            image: require('../../Assets/Images/traditionalPackage.png'),
+            processingFees: '15,000',
+            salary: '1,500',
+            processingWeeks: [3, 4]
+        },
+        {
+            id: 1,
+            name: 'Temporary Employment',
+            image: require('../../Assets/Images/temporaryPackage.png'),
+            processingFees: '0',
+            salary: '3,500',
+            processingWeeks: [3, 4]
+        },
+        {
+            id: 2,
+            name: 'Flexible Employment',
+            image: require('../../Assets/Images/flexiblePackage.png'),
+            processingFees: '0',
+            salary: '3,500',
+            processingWeeks: [3, 4]
+        },
+    ]
+
     constructor(props) {
         super(props);
         this.state = {
@@ -46,16 +73,19 @@ export default class WorkerProfile extends Component {
             workerFlag: require('../../Assets/Images/Canada.png'),
             workerName: 'ANNISA BINT SABER',
             workerExperience: 10,
-            nationality:'Canadian',
-            religion:'Muslim',
-            weight:'82',
+            nationality: 'Canadian',
+            religion: 'Muslim',
+            weight: '82',
             maritalStatus: 'Married',
-            birthDate:'25/07/1986',
-            age:'33',
-            height:'182',
-            children:2,
-            summary:'Trustworthy, friendly and highly reliable maid with considerable experience in running a largehousehold, pays extreme attention to every detail and ensures high standards.',
-            areasOfExperience: this.areasOfExperience
+            birthDate: '25/07/1986',
+            age: '33',
+            height: '182',
+            children: 2,
+            summary: 'Trustworthy, friendly and highly reliable maid with considerable experience in running a largehousehold, pays extreme attention to every detail and ensures high standards.',
+            areasOfExperience: this.areasOfExperience,
+            languages: ['Arabic', 'English', 'Indian'],
+            workLocations: ['UAE', 'Egypt', 'Malaysia', 'Indonesia'],
+            selectedPackage: null
 
         }
     }
@@ -92,7 +122,7 @@ export default class WorkerProfile extends Component {
 
     _renderWorkerNameSection = () => {
         return (
-            <View style={{ width: '100%', alignItems: 'center', marginVertical:RFValue(32) }}>
+            <View style={{ width: '100%', alignItems: 'center', marginVertical: RFValue(32) }}>
                 <Text style={{ textAlign: "center", fontFamily: Fonts.apercuBold, fontSize: RFValue(14), color: '#707070' }}>{this.state.workerName}</Text>
                 <Text style={{ textAlign: 'center', fontFamily: Fonts.apercu, fontSize: RFValue(14), color: Colors.mainColor }}>{this.state.workerExperience} years of Experience</Text>
             </View>
@@ -129,7 +159,7 @@ export default class WorkerProfile extends Component {
     }
 
     _renderSummarySection = () => {
-        let textStyle = { fontSize: RFValue(12), fontFamily: Fonts.apercuBold, color: '#707070', paddingHorizontal:RFValue(24) }
+        let textStyle = { fontSize: RFValue(12), fontFamily: Fonts.apercuBold, color: '#707070', paddingHorizontal: RFValue(24) }
         return (
             <View style={{ marginBottom: RFValue(24) }} >
                 {this._renderSectionTitle(require('../../Assets/Images/summary.png'), 'SUMMARY', RFValue(15), RFValue(18))}
@@ -149,7 +179,7 @@ export default class WorkerProfile extends Component {
 
     _renderAreasOfExperienceList = () => {
         return (
-            <View>
+            <View style={{ marginBottom: RFValue(24) }}>
                 {this._renderSectionTitle(require('../../Assets/Images/experience.png'), 'AREAS OF EXPERTISE', RFValue(12), RFValue(19))}
 
                 <FlatList
@@ -164,13 +194,82 @@ export default class WorkerProfile extends Component {
                                 <View style={{ borderRadius: RFValue(10), height: RFValue(65), width: RFValue(150), overflow: 'hidden' }}>
                                     <Image resizeMode='cover' source={item.picture} style={{ height: RFValue(65), width: RFValue(150), borderRadius: RFValue(10) }} />
                                 </View>
-                                <View style={{height: RFValue(65), width: RFValue(150), justifyContent:'center', alignItems:'center', zIndex:1000, marginTop:RFValue(-65), backgroundColor:'rgba(0,0,0,0.4)'}}>
+                                <View style={{ height: RFValue(65), width: RFValue(150), justifyContent: 'center', alignItems: 'center', zIndex: 1000, marginTop: RFValue(-65), backgroundColor: 'rgba(0,0,0,0.4)' }}>
                                     <Text style={{ fontSize: RFValue(14), fontFamily: Fonts.apercuBold, color: 'white' }}>{item.name}</Text>
                                 </View>
                             </View>)
                     }} />
 
             </View>
+        )
+    }
+
+    _renderSpokenLanguageSection = () => {
+        let textStyle = { fontSize: RFValue(12), fontFamily: Fonts.apercuBold, color: '#707070', paddingHorizontal: RFValue(24) }
+        return (
+            <View style={{ marginBottom: RFValue(24) }}>
+                {this._renderSectionTitle(require('../../Assets/Images/languagesIcon.png'), 'SPOKEN LANGUAGES', RFValue(15), RFValue(18))}
+                <Text style={textStyle}>{this.state.languages.join(' . ')}</Text>
+            </View>
+        )
+    }
+
+    _renderPreviousWorkLocations = () => {
+        let textStyle = { fontSize: RFValue(12), fontFamily: Fonts.apercuBold, color: '#707070', paddingHorizontal: RFValue(24) }
+        return (
+            <View style={{ marginBottom: RFValue(36) }}>
+                {this._renderSectionTitle(require('../../Assets/Images/locationIcon.png'), 'PREVIOUS WORK LOCATIONS', RFValue(15), RFValue(18))}
+                <Text style={textStyle}>{this.state.workLocations.join(' . ')}</Text>
+            </View>
+        )
+    }
+
+    _renderPackageCard = (item) => {
+        let isPackageSelected = this.state.selectedPackage === item.name;
+        let fontStyle = { fontSize: RFValue(14), fontFamily: Fonts.apercuMedium, color: '#707070', paddingHorizontal: RFValue(6), marginBottom: RFValue(8) }
+        return (
+            <TouchableOpacity onPress={() => this.setState({ selectedPackage: item.name })} style={{ marginBottom: RFValue(12), marginHorizontal: RFValue(4), backgroundColor: isPackageSelected ? 'rgba(70,55,149,0.1)' : 'white', borderRadius: RFValue(10), shadowColor: '#707070', shadowOffset: { width: 0, height: 4 }, shadowOpacity: 0.7, elevation: 3, padding: RFValue(12), justifyContent: 'center', alignItems: 'center', width: Dimensions.get('window').width * 0.6 }}>
+                <Image resizeMode='contain' source={item.image} style={{ width: Dimensions.get('window').width * 0.3, height: Dimensions.get('window').height * 0.15 }} />
+                <Text style={{ fontFamily: Fonts.apercuBold, fontSize: RFValue(14), color: '#707070', marginBottom: RFValue(6) }}>{item.name}</Text>
+                <View style={{ flexDirection: 'row', width:'100%' }}>
+                    <Text style={fontStyle}>&#8226;</Text>
+                    <Text style={fontStyle}>Pay AED {item.processingFees} processing fees to bring and host a domestic worker from outside UAE.</Text>
+                </View>
+                <View style={{ flexDirection: 'row', width:'100%' }}>
+                    <Text style={fontStyle}>&#8226;</Text>
+                    <Text style={fontStyle}>Pay AED {item.salary} monthly salary</Text>
+                </View>
+                <View style={{ flexDirection: 'row', width:'100%' }}>
+                    <Text style={fontStyle}>&#8226;</Text>
+                    <Text style={fontStyle}>Processing takes {item.processingWeeks.join(' - ')} weeks</Text>
+                </View>
+                <View style={{ marginVertical: RFValue(6), width: RFValue(24), height: RFValue(24), borderRadius: RFValue(12), borderWidth: 1, borderColor: '#707070', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ width: RFValue(16), height: RFValue(16), borderRadius: RFValue(8), padding: RFValue(4), backgroundColor: isPackageSelected ? '#463795' : 'white' }} />
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    _renderPackagesContainer = () => {
+        return (
+            <View>
+                <Text style={{ marginBottom: RFValue(16), fontSize: RFValue(18), fontFamily: Fonts.apercuBold, color: '#463795', alignSelf: 'center' }}>{this.state.selectedPackage ? this.state.selectedPackage : 'SELECT PACKAGE'}</Text>
+                <FlatList
+                    horizontal={true}
+                    data={this.packages}
+                    keyExtractor={(item) => item.id.toString()}
+                    showsHorizontalScrollIndicator={false}
+                    //style={{ maxHeight: Dimensions.get('window').height * 0.4 }}
+                    renderItem={({ item }) => this._renderPackageCard(item)} />
+            </View>
+        )
+    }
+
+    _renderSubmitButton = () => {
+        return (
+            <TouchableOpacity style={{ marginTop: RFValue(16), borderRadius: RFValue(25), height: RFValue(50), width: Dimensions.get('window').width * 0.9, backgroundColor: '#463795', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 18, fontFamily: Fonts.apercuMedium, color: 'white' }}>PROCEED</Text>
+            </TouchableOpacity>
         )
     }
 
@@ -185,6 +284,10 @@ export default class WorkerProfile extends Component {
                     {this._renderSummarySection()}
                     {this._renderVideoLink()}
                     {this._renderAreasOfExperienceList()}
+                    {this._renderSpokenLanguageSection()}
+                    {this._renderPreviousWorkLocations()}
+                    {this._renderPackagesContainer()}
+                    {this._renderSubmitButton()}
                 </ScrollView>
             </View>
         )
